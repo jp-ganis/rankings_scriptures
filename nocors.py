fa5012175fd76b05354603141d0e61a940047493
Jp Ganis <james.ganis@gmail.com>
2020-10-18 12:20:39 +0100

    cleaning up i guess

--- nocors.py
+++ nocors.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.call(r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe -disable-web-security --user-data-dir=C:\Users\James\Desktop\rankings_scriptures\nocors-data-dir')
+subprocess.call(r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe -disable-web-security --user-data-dir=C:\Program Files (x86)\Google\Chrome\Application')
 ##.\chrome.exe 
